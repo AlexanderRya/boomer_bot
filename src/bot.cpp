7e2c573e8eddb4eeb54b956a8e1863b04515454a
AlexanderRya <alexandr.benbaccar@gmail.com>
2020-02-02 23:26:41 +0100

    Added a thingy

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,145 +1,182 @@
 #include "bot.hpp"
 
 namespace bot {
-	bot::bot(std::string token, const char prefix)
-	: token(std::move(token)),
-	prefix(prefix) {
-		internal_event_map["READY"] = [this](const nlohmann::json& j) {
-			heartbeat = std::thread([this]() {
-				nlohmann::json data{
-					{ "op", 1 },
-					{ "d", nullptr }
-				};
-				if (last_sequence_id != -1) {
-					data["d"] = last_sequence_id;
-				}
-				while (!connection_closed) {
-					con->send(data.dump());
-					std::cout << "sent opcode 11\n";
-					acked = false;
-					std::this_thread::sleep_for(std::chrono::milliseconds(interval));
-					if (!acked) {
-						std::cout << "heartbeat error, trying to reconnect...\n";
-						disconnected = true;
-						data = nlohmann::json({ { "token", this->token },
-												  { "session_id", session_id },
-												  { "seq", last_sequence_id } });
-						con->send(data.dump());
-					}
-				}
-			});
-			session_id = j["d"]["session_id"].get<std::string>();
-			name = j["d"]["user"]["username"].get<std::string>();
-			discriminator = std::stoi(j["d"]["user"]["discriminator"].get<std::string>());
-			std::cout << fmt::format("READY\nLogged in as: {}#{}\n\n", name, discriminator);
-		};
+    bot::bot(std::string token, const char prefix)
+        : token(std::move(token)),
+          prefix(prefix) {
+        internal_event_map["READY"] = [this](const nlohmann::json& j) {
+            heartbeat = std::thread([this]() {
+                while (!connection_closed) {
+                    nlohmann::json data{
+                        { "op", 1 },
+                        { "d", nullptr }
+                    };
+                    if (last_sequence_id != -1) {
+                        data["d"] = last_sequence_id;
+                    }
+                    con->send(data.dump());
+                    std::cout << "sent opcode 11\n";
+                    acked = false;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+                    if (!acked) {
+                        std::cout << "\n\n[Error] Reconnecting...\n\n";
+                        connection_closed = true;
+                    }
+                }
+            });
+            session_id = j["d"]["session_id"].get<std::string>();
+            name = j["d"]["user"]["username"].get<std::string>();
+            discriminator = std::stoi(j["d"]["user"]["discriminator"].get<std::string>());
+            std::cout << fmt::format("READY\nLogged in as: {}#{}\n\n", name, discriminator);
+        };
+    }
 
-		c.set_access_channels(websocketpp::log::alevel::all);
-		c.clear_access_channels(websocketpp::log::alevel::frame_payload);
-		c.set_error_channels(websocketpp::log::elevel::all);
-		c.init_asio();
+    void bot::init_gateway() {
+        static bool is_init = false;
 
-		c.set_tls_init_handler([](const ws_pp::connection_hdl&) {
-			auto ctx = std::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::sslv23);
-			ctx->set_options(boost::asio::ssl::context::default_workarounds |
-							 boost::asio::ssl::context::no_sslv2 |
-							 boost::asio::ssl::context::no_sslv3 |
-							 boost::asio::ssl::context::single_dh_use);
-			return ctx;
-		});
-		c.set_message_handler([this](const websocketpp::connection_hdl& hdl, const message_ptr& msg) {
-			nlohmann::json j = nlohmann::json::parse(msg->get_payload());
-			switch (j["op"].get<int>()) {
-				case 9: {
-					if (disconnected) {
-						std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-						con->send(util::get_identify_packet(this->token));
-						std::cout << "successfully reconnected\n";
-					}
-				}
-				case 10: {
-					std::cout << "opcode 10 received\n";
-					interval = j["d"]["heartbeat_interval"].get<int>();
-					con->send(util::get_identify_packet(this->token));
-				} break;
-				case 11: {
-					std::cout << "acked! json: " << j << "\n";
-					acked = true;
-				} break;
-				default: {
-					last_sequence_id = j["s"].is_number() && j.contains("s") ? j["s"].get<int>() : -1;
-					auto event = j["t"].get<std::string>();
-					if (internal_event_map.contains(event)) {
-						internal_event_map[event](j);
-					} else if (event_map.contains(event)) {
-						event_map[event](j);
-					} else {
-						//std::cout << event << " (unhandled)\n";
-					}
-				} break;
-			}
-		});
-		c.start_perpetual();
+        std::string uri = util::get_ws_url(this->token);
 
-		ws_pp::lib::error_code ec;
-		con = c.get_connection(util::get_ws_url(this->token), ec);
+        c.set_access_channels(websocketpp::log::alevel::all);
+        c.clear_access_channels(websocketpp::log::alevel::frame_payload);
+        c.set_error_channels(websocketpp::log::elevel::all);
 
-		if (ec) {
-			throw std::runtime_error("connect initialization error: " + ec.message());
-		}
+        if (!is_init) {
+            c.init_asio();
+            is_init = true;
+        }
 
-		background_ws = std::thread(&client::run, &c);
+        c.start_perpetual();
+        perpetual = std::thread(&client::run, &c);
 
-		c.connect(con);
-	}
+        c.set_message_handler([this](const websocketpp::connection_hdl &, const message_ptr &msg) {
+            nlohmann::json j = nlohmann::json::parse(msg->get_payload());
+            switch (j["op"].get<int>()) {
+                case 10: {
+                    //std::cout << "opcode 10 received\n";
+                    interval = j["d"]["heartbeat_interval"].get<int>();
+                    con->send(util::get_identify_packet(this->token));
+                } break;
 
-	void bot::run() {
-		using namespace std::chrono_literals;
-		c.run();
-		std::this_thread::sleep_for(1ms);
-		heartbeat.join();
-	}
+                case 11: {
+                    std::cout << "acked! json: " << j << "\n";
+                    acked = true;
+                } break;
 
-	void bot::push_on_command_event(const std::function<void(const nlohmann::json&)>& f) {
-		event_map["MESSAGE_CREATE"] = f;
-	}
+                default: {
+                    last_sequence_id = j.contains("s") ? j["s"].get<int>() : -1;
+                    auto event = j["t"].get<std::string>();
+                    if (internal_event_map.contains(event)) {
+                        internal_event_map[event](j);
+                    } else if (event_map.contains(event)) {
+                        event_map[event](j);
+                    } else {
+                        //std::cout << event << " (unhandled)\n";
+                    }
+                } break;
+            }
+        });
 
-	void bot::send_message(const types::snowflake& channel_id, const std::string& content) const {
-		auto body = nlohmann::json{
-			{ "content", content },
-			{ "tts", false }
-		};
-		//std::cout << body << "\n\n";
-		auto r = cpr::Post(cpr::Url{ util::get_api_url() + "/channels/" + channel_id + "/messages" },
-			cpr::Header{
-				{ "Content-Type", "application/json" },
-				{ "User-Agent", util::user_agent },
-				{ "Authorization", "Bot " + token }
-			},
-			cpr::Body{ body.dump() });
-		std::cout << "send_message status code: " << r.status_code << "\n" << "body: " << r.text << "\n";
-	}
+        c.set_tls_init_handler([](const websocketpp::connection_hdl &) {
+            auto ctx = std::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::sslv23);
+            try {
+                ctx->set_options(boost::asio::ssl::context::default_workarounds |
+                                 boost::asio::ssl::context::no_sslv2 |
+                                 boost::asio::ssl::context::no_sslv3 |
+                                 boost::asio::ssl::context::single_dh_use);
+                ctx->set_verify_mode(boost::asio::ssl::verify_peer);
+                ctx->set_verify_callback([](const bool preverified, boost::asio::ssl::verify_context &ictx) {
+                    return util::verify_certificate("discord.gg", preverified, ictx);
+                });
+                ctx->load_verify_file("../ca-chain.cert.pem");
+            } catch (std::exception& e) {
+                std::cout << e.what() << "\n";
+            }
+            return ctx;
+        });
 
-	void bot::ping(const types::snowflake& channel_id) {
-		using namespace std::chrono_literals;
-		can_use_ping = check_ping_cooldown();
-		if (can_use_ping) {
-			std::stringstream s{};
-			if (reminder_list.empty()) {
-				send_message(channel_id, "No one is on the reminder list!");
-			}
-			for (const auto& [username, id] : reminder_list) {
-				s << fmt::format("<@!{}> ", id);
-			}
-			send_message(channel_id, s.str());
-			can_use_ping = false;
-			next_ping = std::time(nullptr) + 21600;
-		} else {
-			send_message(channel_id, "You can't do that right now!");
-		}
-	}
+        ws_pp::lib::error_code ec;
+        con = c.get_connection(uri, ec);
 
-	bool bot::check_ping_cooldown() const {
-		return std::time(nullptr) >= next_ping;
-	}
+        if (ec) {
+            throw std::runtime_error("connect initialization error: " + ec.message());
+        }
+
+        c.connect(con);
+    }
+
+    void bot::run() {
+        using namespace std::chrono_literals;
+
+        init_gateway();
+
+        while (true) {
+            std::this_thread::sleep_for(10ms);
+            if (connection_closed) {
+                con->pause_reading();
+                con->close(ws_pp::close::status::service_restart, "Success");
+                con.reset();
+
+                std::this_thread::sleep_for(20s);
+
+                heartbeat.join();
+
+                ws_pp::lib::error_code ec;
+                con = c.get_connection(util::get_ws_url(this->token), ec);
+
+                if (ec) {
+                    throw std::runtime_error("connect initialization error: " + ec.message());
+                }
+
+                c.connect(con);
+
+                acked = true;
+                connection_closed = false;
+            }
+        }
+    }
+
+    void bot::push_on_command_event(const std::function<void(const nlohmann::json&)>& f) {
+        event_map["MESSAGE_CREATE"] = f;
+    }
+
+    void bot::send_message(const types::snowflake& channel_id, const std::string& content) const {
+        auto body = nlohmann::json{
+            { "content", content },
+            { "tts", false }
+        };
+        //std::cout << body << "\n\n";
+        auto r = cpr::Post(cpr::Url{ util::get_api_url() + "/channels/" + channel_id + "/messages" },
+                           cpr::Header{
+                               { "Content-Type", "application/json" },
+                               { "User-Agent", util::user_agent },
+                               { "Authorization", "Bot " + token }
+                           },
+                           cpr::Body{ body.dump() });
+        std::cout << "send_message status code: " << r.status_code << "\n" << "body: " << r.text << "\n";
+    }
+
+    void bot::ping(const types::snowflake& channel_id) {
+        using namespace std::chrono_literals;
+        can_use_ping = check_ping_cooldown();
+        if (can_use_ping) {
+            std::stringstream s{};
+            if (reminder_list.empty()) {
+                send_message(channel_id, "No one is on the reminder list!");
+            }
+            for (const auto& [username, id] : reminder_list) {
+                s << fmt::format("<@!{}> ", id);
+            }
+            send_message(channel_id, s.str());
+            can_use_ping = false;
+            std::ofstream("../time.txt", std::ios::out | std::ios::trunc) << (std::time(nullptr) + 21600);
+        } else {
+            send_message(channel_id, "You can't do that right now!");
+        }
+    }
+
+    bool bot::check_ping_cooldown() const {
+        std::time_t next_ping{};
+        std::ifstream("../time.txt") >> next_ping;
+        return std::time(nullptr) >= next_ping;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,85 @@
 #include "bot.hpp"
 
 int main() {
-	const std::string daknig_id = "160412986597441537";
-	std::ifstream f("../token.txt");
-	std::string token{ std::istreambuf_iterator{ f }, {} };
-	bot::bot b(std::move(token), '%');
-	bot::util::deserialize(b.reminder_list);
-	b.push_on_command_event([&b, &daknig_id](const nlohmann::json& j) {
-		auto content = bot::util::get_from_json<std::string>(j["d"], "content");
-		if (content[0] == b.prefix /*&& channel_id == "526518219549442071"*/) {
-			auto author_id = bot::util::get_from_json<std::string>(j["d"]["author"], "id");
-			auto author_username = bot::util::get_from_json<std::string>(j["d"]["author"], "username");
-			auto channel_id = bot::util::get_from_json<std::string>(j["d"], "channel_id");
-			auto guild_id = bot::util::get_from_json<std::string>(j["d"], "guild_id");
-			if (content.substr(1, 3) == "sub") {
-				if (content.size() > 4 && content.substr(4) == " to daily_reminder") {
-					if (std::find_if(b.reminder_list.begin(), b.reminder_list.end(), [&](const auto& user) {
-						return user.second == author_id;
-					}) != b.reminder_list.end()) {
-						b.send_message(channel_id, "You already are on the reminder list!");
-					} else {
-						b.send_message(channel_id, "Successfully subbed to the daily reminder!");
-						b.reminder_list.emplace_back(author_username, author_id);
-						bot::util::serialize(b.reminder_list);
-					}
-				} else {
-					b.send_message(channel_id, "Invalid syntax, try: `%sub to daily_reminder`");
-				}
-			} else if (content.substr(1, 4) == "ping") {
-				if (content.size() > 5 && content.substr(5) == " subs daily_reminder") {
-					if (author_id == daknig_id) {
-						b.ping(channel_id);
-					} else {
-						b.send_message(channel_id, "Only our lord and saviour DaKnig can use this command");
-					}
-				} else {
-					b.send_message(channel_id, "Invalid syntax, try: `%ping subs daily_reminder`");
-				}
-			} else if (content.substr(1, 5) == "unsub") {
-				auto it = std::find_if(b.reminder_list.begin(), b.reminder_list.end(), [&](const auto& user) {
-					return user.second == author_id;
-				});
-				if (it != b.reminder_list.end()) {
-					b.reminder_list.erase(it);
-					b.send_message(channel_id, "Successfully unsubbed to the daily reminder!");
-				} else {
-					b.send_message(channel_id, "You are not present on the reminder list!");
-				}
-			} else if (content.substr(1, 4) == "help") {
-				b.send_message(channel_id,
-							   "Command list:\n"
-							   "%help: basically this command duh\n"
-							   "%sub: syntax: `%sub to daily_reminder`, you choose to get pinged to get reminded that: yall dum\n"
-							   "%ping: syntax: `%ping subs daily_reminder`, only DaKnig's allowed to use this command, it pings everyone on the list (has a 6 hours cooldown)\n"
-							   "%unsub: syntax: `%unsub`, unsubs from this service (:feelsbad:)\n"
-							   "%list: syntax: `%list`, shows everyone subscribed to the service\n"
-				);
-			} else if (content.substr(1, 4) == "list") {
-				if (b.reminder_list.empty()) {
-					b.send_message(channel_id, "No one is on the list!");
-				} else {
-					std::stringstream s;
-					s << "List of the members of the daily reminder:\n";
-					for (const auto&[username, id] : b.reminder_list) {
-						s << username << "\n";
-					}
-					b.send_message(channel_id, s.str());
-				}
-			} else {
-				b.send_message(channel_id, "Invalid command");
-			}
-		}
-	});
-	b.run();
-	return 0;
+    const std::string daknig_id = "248901263054471168";
+    std::ifstream f("../token.txt");
+    std::string token{ std::istreambuf_iterator{ f }, {}};
+    bot::bot b(std::move(token), '%');
+    bot::util::deserialize(b.reminder_list);
+    b.push_on_command_event([&b, &daknig_id](const nlohmann::json& j) {
+        auto content = bot::util::get_from_json<std::string>(j["d"], "content");
+        if (content[0] == b.prefix /*&& channel_id == "526518219549442071"*/) {
+            auto author_id = bot::util::get_from_json<std::string>(j["d"]["author"], "id");
+            auto author_username = bot::util::get_from_json<std::string>(j["d"]["author"], "username");
+            auto channel_id = bot::util::get_from_json<std::string>(j["d"], "channel_id");
+            auto guild_id = bot::util::get_from_json<std::string>(j["d"], "guild_id");
+            if (content.substr(1, 3) == "sub") {
+                if (content.size() > 4 && content.substr(4) == " to daily_reminder") {
+                    if (std::find_if(b.reminder_list.begin(), b.reminder_list.end(), [&](const auto& user) {
+                        return user.second == author_id;
+                    }) != b.reminder_list.end()) {
+                        b.send_message(channel_id, "You already are on the reminder list!");
+                    } else {
+                        b.send_message(channel_id, "Successfully subbed to the daily reminder!");
+                        b.reminder_list.emplace_back(author_username, author_id);
+                        bot::util::serialize(b.reminder_list);
+                    }
+                } else {
+                    b.send_message(channel_id, "Invalid syntax, try: `%sub to daily_reminder`");
+                }
+            } else if (content.substr(1, 4) == "ping") {
+                if (author_id == daknig_id || author_id == "187927238274973696") {
+                    if (content.size() > 5 && content.substr(5) == " subs daily_reminder") {
+                        b.ping(channel_id);
+                    } else {
+                        b.send_message(channel_id, "Invalid syntax, try: `%ping subs daily_reminder`");
+                    }
+                } else {
+                    b.send_message(channel_id, "Only our lord and saviour DaKnig can use this command");
+                }
+            } else if (content.substr(1, 5) == "unsub") {
+                if (content.size() == 6) {
+                    auto it = std::find_if(b.reminder_list.begin(), b.reminder_list.end(), [&](const auto& user) {
+                        return user.second == author_id;
+                    });
+                    if (it != b.reminder_list.end()) {
+                        b.reminder_list.erase(it);
+                        bot::util::serialize(b.reminder_list);
+                        b.send_message(channel_id, "Successfully unsubbed to the daily reminder!");
+                    } else {
+                        b.send_message(channel_id, "You are not present on the reminder list!");
+                    }
+                } else {
+                    b.send_message(channel_id, "Invalid syntax, try: `%unsub`");
+                }
+            } else if (content.substr(1, 4) == "help") {
+                b.send_message(channel_id,
+                               "Command list:\n"
+                               "%help: basically this command duh\n"
+                               "%sub: syntax: `%sub to daily_reminder`, you choose to get pinged to get reminded that: yall dum\n"
+                               "%ping: syntax: `%ping subs daily_reminder`, only DaKnig's allowed to use this command, it pings everyone on the list (has a 6 hours cooldown)\n"
+                               "%unsub: syntax: `%unsub`, unsubs from this service (:feelsbad:)\n"
+                               "%list: syntax: `%list`, shows everyone subscribed to the service\n"
+                               "%ask: a glorious command."
+                );
+            } else if (content.substr(1, 4) == "list") {
+                if (b.reminder_list.empty()) {
+                    b.send_message(channel_id, "No one is on the list!");
+                } else {
+                    std::stringstream s;
+                    s << "List of the members of the daily reminder:\n";
+                    for (const auto& [username, id] : b.reminder_list) {
+                        s << username << "\n";
+                    }
+                    b.send_message(channel_id, s.str());
+                }
+            } else if (content.substr(1, 3) == "ask") {
+                b.send_message(channel_id, "Rules §2.1: **Actually ask your question.** Posting \"Can somebody help me?\" or similar messages is annoying and doesn't help anyone, including you.");
+            } else {
+                b.send_message(channel_id, "Invalid command");
+            }
+        }
+    });
+    b.run();
+    return 0;
 }
